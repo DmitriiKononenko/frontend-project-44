70aaa80b06b2a47a812e854df84c8a06c2d5a30c
dmitrii kononenko <dpkdpk1992@gmail.com>
2023-01-26 23:42:35 +0300

    bug fixing

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const name = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+const name = () => {
+  const yourName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${yourName}!`);
 };
+export default name;
